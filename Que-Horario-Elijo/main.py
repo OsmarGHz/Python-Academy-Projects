597d9bf7069a13d86ec0380e7de76332e1dceefd
OsmarGHz <osmarjavier1@gmail.com>
2025-05-01 20:58:19 -0600

    Reordered folders

--- Que-Horario-Elijo/main.py
+++ Que-Horario-Elijo/main.py
@@ -1,16 +1,16 @@
-import os.path;
-import datetime as dt;
+import os.path
+import datetime as dt
 
-from google.auth.transport.requests import Request;
-from google.oauth2.credentials import Credentials;
-from google_auth_oauthlib.flow import InstalledAppFlow;
-from googleapiclient.discovery import build;
-from googleapiclient.errors import HttpError;
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
 def main():
-    creds = None;
+    creds = None
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json")
 
